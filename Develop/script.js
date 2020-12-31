24157eeca4d32ddf2303893e3f8afea5f060adbb
rhandom1 <rhandomone@gmail.com>
2020-12-30 22:10:50 -0600

    More variables created and confirmations added.

--- Develop/script.js
+++ Develop/script.js
@@ -7,40 +7,61 @@ var allLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specialChar =  ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 
-var confirmLength = "";
+// Criteria options
+var confirmLength;
 var confirmSpecialChar;
 var confirmNumber;
 var confirmUpper;
 var confirmLower;
 
-// A seperate variable to begin the user query?
-var get = document.querySelector(#generate);
+// parseInt
 
-// Do we need a seperate event?
+function userOptions () {
+  var confirmLength = parseInt(prompt("Your password must be between 8 and 128 characters. Please enter a whole number to set the length of your password."));
+   
+// break into separate
+// Confirm number
+// adding return ends the function
 
-// It seems we need to write out the generatePassword function to be called below? 
-// First prompt is to set the length. Recalling comfirm variable?
-function generatePassword() {
-  var confirmLength = (prompt("Your password must be between 8 and 128 characters. Please enter a whole number to set teh length of your password."));
-   }
-
-   If (confirmLength > 8 || confirmLength < 128) {
-     alert("Please enter a number between 8 and 128.");
-   }
-   else {
-     confirmUpper = confirm("Do you wish to include Uppercase letters?");
-     confirmLower = confirm("Do you wish to include Lowercase letters?");
-     confirmNumber = confirm("Do you wish to include Numbers?");
-     confirmSpecialChar = confirm("Do you wish to include Special Characters such as '!' or '%'");
-   };
+  
+  // Once length is confirmed, user can add criteria
+  // making a confirm as a var will hold the response
+   
+     confirmUpper = confirm("Do you wish to include Uppercase letters? Click OK to confirm.");
+     confirmLower = confirm("Do you wish to include Lowercase letters? Click OK to confirm.");
+     confirmNumber = confirm("Do you wish to include Numbers? Click OK to confirm.");
+     confirmSpecialChar = confirm("Do you wish to include Special Characters such as '!' or '%'? Click OK to confirm.");
+   
 
   //  Need more else/if for no choice? Can the confirm option be greyed out until a choice is made?
   if (!confirmUpper && !confirmLower && !confirmNumber && !confirmSpecialChar) {
-    alert("You must select at least one criteria option. For the highest security, select all four options.")
-  }
+    alert("You must select at least one criteria option. For the highest security, select all four options.");
+    return
+    }
+// Object for choice combination
+    var userChoices = {
+      confirmLength:confirmLength,
+      confirmUpper:confirmUpper,
+      confirmLower:confirmLower,
+      confirmNumber:confirmNumber,
+      confirmSpecialChar:confirmSpecialChar,
+    }
+    return userChoices
+  }   
 
-  // How do we combine the criteria options? 
+  // function is created to call randomly pull a character from the arrays
 
+
+// 3 arrays 1- for password 2- for chosen char 3- characters to include
+
+
+// use . to get to an objects properties
+// make an if statement for each character option
+ 
+// created the loop to check length. If greater than zero keep checking
+ 
+    
+}
 
 // Write password to the #password input
 function writePassword() {
